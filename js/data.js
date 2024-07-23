49d281f11a5d66c9107235b7a7c1b3f588ea20a6
maxbbkCmd <maxbbk.cmd@mail.ru>
2024-07-23 15:31:45 +0300

    Refactored obj property

--- js/data.js
+++ js/data.js
@@ -1,140 +1,139 @@
 const header = {
   defaultLanguage: 'RU',
-  langSelect: [
+  langs: [
     {
-      value: 'ru',
-      text: 'RU'
+      name: 'RU',
+      key: 'ru',
     },
     {
-      value: 'en',
-      text: 'EN'
-    }
+      name: 'EN',
+      key: 'en',
+    },
   ],
-  menu: [
+  navigation: [
     {
-    name: 'Download',
-    url: '#download',
-  },
+      name: 'Download',
+      anchor: '#download',
+    },
     {
-    name: 'Warranty',
-    url: '#warranty',
-  },
+      name: 'Warranty',
+      anchor: '#warranty',
+    },
     {
-    name: 'Care',
-    url: '#care',
-  },
+      name: 'Care',
+      anchor: '#care',
+    },
     {
-    name: 'Cashback',
-    url: '#cashback',
-  },
+      name: 'Cashback',
+      anchor: '#cashback',
+    },
     {
-    name: 'Clients',
-    url: '#clients',
-  },
-]
+      name: 'Clients',
+      anchor: '#clients',
+    },
+  ],
 };
 
 const download = {
   title: 'Peaceful shopping mindful money',
-  texts: ['See how we can help with making your shopping experience and money management more ZEN.'],
+  texts: [
+    'See how we can help with making your shopping experience and money management more ZEN.',
+  ],
   links: [
     {
       name: 'Apple',
-      url: 'https://apple.com'
+      url: 'https://apple.com',
     },
     {
       name: 'Google',
       url: 'https://google.com',
-    }
+    },
   ],
-  images:
+  images: {
+    source: './assets/images/1.jpg',
+    description: 'Bag and device',
+  },
+};
+
+const warranty = {
+  title: '1-year warranty boost',
+  texts: [
+    'ZEN cards have a very neat feature that will help you whenever your electronic devices start failing after the standard warranty ends, which is pretty common.',
+    'When you buy any electronics with your ZEN shopping Mastercard, their warranty gets extended by one year. How does it work? It simply does. Automatically.',
+  ],
+  images: {
+    source: './assets/images/2.jpg',
+    description: 'Basket',
+  },
+};
+
+const care = {
+  title: 'ZEN Care',
+  texts: [
+    "Shopping online is usually a delightful experience. Until it's not.",
+    'Until something you buy, proves to be faulty, missing or a totally wrong item, you’re in for long product return procedures.',
+    'But, here comes ZEN Care: a free, built-in shopping protection.',
+  ],
+  images: {
+    source: './assets/images/3.jpg',
+    description: 'Card',
+  },
+};
+
+const cashback = {
+  title: 'and cashback!',
+  texts: [
+    'It saves a little bit of money while you’re shopping, but in many cases it takes ages to get anything out of it.',
+    'We gave it a thought and made cashback way more friendly.',
+    'Up to 14% of what you spend using ZEN Mastercard ® will materialize on your account right after the purchase.',
+  ],
+  buttonText: 'Order a consultation',
+};
+1;
+const clients = {
+  brands: [
     {
-      source: './assets/images/1.jpg',
-      description: 'Bag and device',
-    }
-  };
-  
-  const warranty = {
-    title: '1-year warranty boost',
-    texts: [
-      'ZEN cards have a very neat feature that will help you whenever your electronic devices start failing after the standard warranty ends, which is pretty common.',
-      'When you buy any electronics with your ZEN shopping Mastercard, their warranty gets extended by one year. How does it work? It simply does. Automatically.'
-    ],
-    images:
-      {
-        source: './assets/images/2.jpg',
-        description: 'Basket',
-      }
-  };
-  
-  const care = {
-    title: 'ZEN Care',
-    texts: [
-      'Shopping online is usually a delightful experience. Until it\'s not.',
-      'Until something you buy, proves to be faulty, missing or a totally wrong item, you’re in for long product return procedures.',
-      'But, here comes ZEN Care: a free, built-in shopping protection.'
-    ],
-    images:
-      {
-        source: './assets/images/3.jpg',
-        description: 'Card',
-      }
-  };
-
-  const cashback = {
-    title: 'and cashback!',
-    texts: [
-      'It saves a little bit of money while you’re shopping, but in many cases it takes ages to get anything out of it.',
-      'We gave it a thought and made cashback way more friendly.',
-      'Up to 14% of what you spend using ZEN Mastercard ® will materialize on your account right after the purchase.'
-    ],
-    buttonText: 'Order a consultation'
-  };
-
-  const clients = {
-    brands: [
-      {
-        lightSource: '.assets/brands/light/1.svg',
-        darkSource: '.assets/brands/dark/1.svg',
-        description: 'Rosegal'
-      },
-      {
-        lightSource: '.assets/brands/light/2.svg',
-        darkSource: '.assets/brands/dark/2.svg',
-        description: 'Geabest'
-      },
-      {
-        lightSource: '.assets/brands/light/3.svg',
-        darkSource: '.assets/brands/dark/3.svg',
-        description: 'Allegro'
-      },
-      {
-        lightSource: '.assets/brands/light/4.svg',
-        darkSource: '.assets/brands/dark/4.svg',
-        description: 'Aliexpress'
-      },
-      {
-        lightSource: '.assets/brands/light/5.svg',
-        darkSource: '.assets/brands/dark/5.svg',
-        description: 'Booking'
-      },
-      {
-        lightSource: '.assets/brands/light/6.svg',
-        darkSource: '.assets/brands/dark/6.svg',
-        description: 'Media Markt'
-      },
-      {
-        lightSource: '.assets/brands/light/7.svg',
-        darkSource: '.assets/brands/dark/7.svg',
-        description: 'Rtv Euro Agd'
-      },
-      {
-        lightSource: '.assets/brands/light/8.svg',
-        darkSource: '.assets/brands/dark/8.svg',
-        description: 'Groupon'
-      },
-    ],
-  };
+      lightSource: '.assets/brands/light/1.svg',
+      darkSource: '.assets/brands/dark/1.svg',
+      description: 'Rosegal',
+    },
+    {
+      lightSource: '.assets/brands/light/2.svg',
+      darkSource: '.assets/brands/dark/2.svg',
+      description: 'Geabest',
+    },
+    {
+      lightSource: '.assets/brands/light/3.svg',
+      darkSource: '.assets/brands/dark/3.svg',
+      description: 'Allegro',
+    },
+    {
+      lightSource: '.assets/brands/light/4.svg',
+      darkSource: '.assets/brands/dark/4.svg',
+      description: 'Aliexpress',
+    },
+    {
+      lightSource: '.assets/brands/light/5.svg',
+      darkSource: '.assets/brands/dark/5.svg',
+      description: 'Booking',
+    },
+    {
+      lightSource: '.assets/brands/light/6.svg',
+      darkSource: '.assets/brands/dark/6.svg',
+      description: 'Media Markt',
+    },
+    {
+      lightSource: '.assets/brands/light/7.svg',
+      darkSource: '.assets/brands/dark/7.svg',
+      description: 'Rtv Euro Agd',
+    },
+    {
+      lightSource: '.assets/brands/light/8.svg',
+      darkSource: '.assets/brands/dark/8.svg',
+      description: 'Groupon',
+    },
+  ],
+};
 
 const footer = {
   columns: [
@@ -143,86 +142,86 @@ const footer = {
       links: [
         {
           name: 'Help center',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
         {
           name: 'Join ZEN team',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
         {
           name: 'Privacy Policy',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
         {
           name: 'Cookies',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
         {
           name: 'Terms of use',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
-      ]
+      ],
     },
     {
       title: 'Discover',
       links: [
         {
           name: 'For developers',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
         {
           name: 'Apple Pay',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
         {
           name: 'Google Pay',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
-      ] 
+      ],
     },
     {
       title: 'Offer',
       links: [
         {
           name: 'Business',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
         {
           name: 'Personal',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
-      ] 
+      ],
     },
     {
       title: 'Language',
       links: [
         {
           name: 'English',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
         {
           name: 'Polish',
-          url: 'https://example.com'
+          url: 'https://example.com',
         },
-      ] 
+      ],
     },
     {
       title: 'Follow us',
       links: [
         {
           url: 'https://example.com',
-          source: '/assets/icon/in.svg'
+          source: '/assets/icon/in.svg',
         },
-      ] 
+      ],
     },
     {
       title: 'Contact',
       links: [
         {
           name: 'hello@zen.com',
-          url: 'mailto:https://example.com'
+          url: 'mailto:https://example.com',
         },
-      ] 
+      ],
     },
   ],
   info: {
@@ -238,6 +237,6 @@ const footer = {
       url: 'https://example.com',
       source: '/assets/icon/developer.svg',
       description: 'Developer',
-    }
-  }
+    },
+  },
 };
