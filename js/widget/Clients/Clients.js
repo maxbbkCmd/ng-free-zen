e66022f0a3cd21c64398766a416162fe338edcae
maxbbkCmd <maxbbkCmd@mail.ru>
2024-09-09 21:19:49 +0300

    Add

--- js/widget/Clients/Clients.js
+++ js/widget/Clients/Clients.js
@@ -16,7 +16,7 @@ export const Clients = (data) => {
       <ul class="clients__brands">
         ${brands.map((brand) => `
           <li class="clients__brand">
-            <img src="${brand.logo.lightSource}" alt="${brand.name}">
+            <img src="${brand.logo.lightSource}" alt="${brand.name}" data-id="brand">
           </li>
           `).join('')}
       </ul>
